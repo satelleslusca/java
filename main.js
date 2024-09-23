5ae6ffc31a4453009a405546b8799e9ec522f8d6
Lucas Satelles <142444442+satelleslusca@users.noreply.github.com>
2024-09-23 09:39:32 -0300

    eqw

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function soma(num1,num2) {
 }
 console.log(soma(2,3));
 
-function areaQuadrado(num1) {
-    return num1**2;
+function areaQuadrado(lado) {
+    return lado**2;
 }
 console.log(areaQuadrado(5));

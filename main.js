f3f301e1331ee6b055aea69102fc518c3881bcdc
Lucas Satelles <142444442+satelleslusca@users.noreply.github.com>
2024-09-23 10:27:45 -0300

    dea

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-// alert('Lucas Satelles e Gustavo')
-function soma(num1,num2) {
-    return num1+num2;
-}
-console.log(soma(2,3));
+// // alert('Lucas Satelles e Gustavo')
+// function soma(num1,num2) {
+//     return num1+num2;
+// }
+// console.log(soma(2,3));
 
-function areaQuadrado(lado) {
-    return lado**2;
-}
-console.log(areaQuadrado(5));
+// function areaQuadrado(lado) {
+//     return lado**2;
+// }
+// console.log(areaQuadrado(5));
 
-function areatriangulo(base,altura){
-    return (base*altura)/2;
-}
-console.log(areatriangulo(8,5));
+// function areatriangulo(base,altura){
+//     return (base*altura)/2;
+// }
+// console.log(areatriangulo(8,5));
+let valor1=prompt('pergunta')

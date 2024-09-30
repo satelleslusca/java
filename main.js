01105e92b09aebfd69777a53bcabf8a1f21929b4
Lucas Satelles <142444442+satelleslusca@users.noreply.github.com>
2024-09-30 09:29:32 -0300

    SA

--- main.js
+++ main.js
@@ -21,4 +21,4 @@
 // }
 // console.log(media(valor1,valor2,valor3));
 // document.write(media(valor1,valor2,valor3));
-let estados = ['Paraná','Fortaleza',]
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']

39f8f629e551a80da46280bd76548a1536972c8f
Lucas Satelles <142444442+satelleslusca@users.noreply.github.com>
2024-09-30 09:14:23 -0300

    ds

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-alert('Lucas Satelles e Gustavo')
-// function soma(num1,num2) {
-//     return num1+num2;
-// }
-// console.log(soma(2,3));
+// alert('Lucas Satelles e Gustavo')
+// // function soma(num1,num2) {
+// //     return num1+num2;
+// // }
+// // console.log(soma(2,3));
 
-// function areaQuadrado(lado) {
-//     return lado**2;
-// }
-// console.log(areaQuadrado(5));
+// // function areaQuadrado(lado) {
+// //     return lado**2;
+// // }
+// // console.log(areaQuadrado(5));
 
-// function areatriangulo(base,altura){
-//     return (base*altura)/2;
+// // function areatriangulo(base,altura){
+// //     return (base*altura)/2;
+// // }
+// // console.log(areatriangulo(8,5));
+// let valor1=parseInt(prompt('Digite o seu primeiro número'));
+// let valor2=parseInt(prompt('Digite o seu segundo número'));
+// let valor3=parseInt(prompt('Digite o seu terceiro número'));
+// function media(valor1,valor2,valor3){
+//     return (valor1+valor2+valor3)/3;
 // }
-// console.log(areatriangulo(8,5));
-let valor1=parseInt(prompt('Digite o seu primeiro número'));
-let valor2=parseInt(prompt('Digite o seu segundo número'));
-let valor3=parseInt(prompt('Digite o seu terceiro número'));
-function media(valor1,valor2,valor3){
-    return (valor1+valor2+valor3)/3;
-}
-console.log(media(valor1,valor2,valor3));
-document.write(media(valor1,valor2,valor3));
+// console.log(media(valor1,valor2,valor3));
+// document.write(media(valor1,valor2,valor3));
